9c9507be0f57209e7d3e2eb5f8cd65722cc2508d
wookie184 <wookie1840@gmail.com>
2024-10-21 20:01:05 +0100

    Remove loading screen more

--- employeeDashboard.php
+++ employeeDashboard.php
@@ -14,12 +14,12 @@
     <body>
 
       
-        <div class="loading-screen">
+        <!-- <div class="loading-screen">
             <h2>LOGO</h2>
             <div class="loader">
                 <div class="loader-line"></div>
             </div>
-        </div>        
+        </div>         -->
 
         <!--
         <div class="cursor">
