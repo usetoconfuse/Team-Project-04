d5921109efbe0312b4e4249253f98e6f1c43751d
Toby Tischler <toby.tischler@gmail.com>
2024-10-21 21:31:54 +0100

    Removed CDN implementation of DIN font Added php include for manager dashboard content

---
+++ managerDashContent.php
@@ -0,0 +1,12 @@
+<!-- Left half: projects list with %s, right half: detailed proj info -->
+
+<link rel="stylesheet" href="managerDashContent.css">
+
+<div id="wrapper">
+    <div id="projectList">
+        <h1>Hello</h1>
+    </div>
+    <div id="projectDetails">
+        <h1>Hello</h1>
+    </div>
+</div>
