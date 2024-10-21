5c762cbb6a09b9e2a501fddaea35624c41a5ad00
wookie184 <wookie1840@gmail.com>
2024-10-21 23:41:12 +0100

    Add knowledgebase

--- knowledgeBase.php
+++ knowledgeBase.php
@@ -1,15 +1,39 @@
-<div>
-    <h2>Knowledge Base</h2>
-</div>
-<div>
-    <div class="kb-createpost">
-        <input type="text" placeholder="Create a post">
-        <button>+</button>
-    </div>
-    <div class="kb-posts">
-        <div class="kb-post">
-            <h1>Post topic</h1>
-            <p>Post content</p>
+<div class="kb-content">
+    <h1 class="kb-title">Knowledge Base</h1>
+
+    <div class="kb-main">
+        <div class="kb-listing">
+            <div class="kb-createpost">
+                <input type="text" placeholder="Create a post">
+                <button>+</button>
+            </div>
+            <div class="kb-posts">
+                <?php
+                for ($i = 0; $i < 3; $i++) {
+                    echo '<div class="kb-post">
+                <h1 class="kb-post-title">Post title ' . ($i + 1) . '</h1>
+                <div class="kb-post-info">
+                    <div class="kb-post-avatar">
+                        <i class="fa-solid fa-user"></i>
+                    </div>
+                    <div class="kb-post-topic">
+                        Post Topic
+                    </div>
+                </div>
+                <div class="kb-post-divider"></div>
+                <p class="kb-post-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ' . ($i + 1) . '</p>
+              </div>';
+                }
+                ?>
+            </div>
+        </div>
+        <div class="kb-sidebar">
+            <h3 class="kb-post-title">Filter by Topic</h3>
+            <ul>
+                <li class="kb-topic">Topic 1</li>
+                <li class="kb-topic">Topic 2</li>
+                <li class="kb-topic">Topic 3</li>
+            </ul>
         </div>
     </div>
 </div>
