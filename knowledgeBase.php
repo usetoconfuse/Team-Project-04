1d23346b48b6bac1c7fe8957809a6f551c168a07
wookie184 <wookie1840@gmail.com>
2024-10-21 19:56:50 +0100

    Add CSS for knowledgebase

--- knowledgeBase.php
+++ knowledgeBase.php
@@ -1,1 +1,15 @@
-hello world
+<div>
+    <h2>Knowledge Base</h2>
+</div>
+<div>
+    <div class="kb-createpost">
+        <input type="text" placeholder="Create a post">
+        <button>+</button>
+    </div>
+    <div class="kb-posts">
+        <div class="kb-post">
+            <h1>Post topic</h1>
+            <p>Post content</p>
+        </div>
+    </div>
+</div>
--- managerDashboard.php
+++ managerDashboard.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="navbar.css"></link>
+        <link rel="stylesheet" href="knowledgeBase.css"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" crossorigin="anonymous" />
         <link href="https://fonts.cdnfonts.com/css/din-alternate" rel="stylesheet">
         <title>Navbar Template</title>
