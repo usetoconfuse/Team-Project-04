02b0f8dcdf52598d2e499de7e845292ef1f96ec9
Toby Tischler <toby.tischler@gmail.com>
2024-10-23 13:32:56 +0100

    Implemented manager dashboard CSS grid

--- managerDashContent.php
+++ managerDashContent.php
@@ -3,12 +3,10 @@
 <link rel="stylesheet" href="managerDashContent.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" crossorigin="anonymous" />
 
-<div id="wrapper">
-    <div id="left">
-        <div id="projectListTitle">
-            <h1>Your Projects</h1>
-        </div>
-        <div id="projectList">
+<div id="grid">
+    <div id="projectList">
+        <h1>Your Managed Projects</h1>
+        <div id="projectListItems">
             <div class="projectListItem active">
                 <h3>Project #1</h3>
                 <i class="fa fa-solid fa-arrow-right"></i>
@@ -27,17 +25,14 @@
             </div>
         </div>
     </div>
-    <div id="right">
-        <div id="topright">
-            <div class="details" id="projectProgressBar">
-                <h1>Progress Bar</h1>
-            </div>
-            <div class="details" id="projectTeamMembers">
-                <h1>Team Members</h1>
-            </div>
-        </div>
-        <div class="details" id="projectContributionGraph">
-            <h1>Project Contribution Graph</h1>
-        </div>
+
+    <div id="projectProgressBar">
+        <h1>Progress Bar</h1>
+    </div>
+    <div id="projectTeamMembers">
+        <h1>Team Members</h1>
+    </div>
+    <div id="projectContributionGraph">
+        <h1>Project Contribution Graph</h1>
     </div>
 </div>
