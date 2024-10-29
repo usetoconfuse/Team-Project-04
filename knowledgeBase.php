b745f39e7d70361b84e4605ab6a3accb371f621a
wookie184 <wookie1840@gmail.com>
2024-10-29 22:53:52 +0000

    Run formatter on knowledgeBase.php

--- knowledgeBase.php
+++ knowledgeBase.php
@@ -20,7 +20,7 @@
                 <?php
                 for ($i = 0; $i < 4; $i++) {
                     //set id for each post
-                    $id = ($i % 2 ==0) ? "technical" : "nonTechnical";
+                    $id = ($i % 2 == 0) ? "technical" : "nonTechnical";
                     switch ($i) {
                         case 0:
                             $key = "coding-standards";
@@ -34,20 +34,23 @@
                         case 3:
                             $key = "workplace-hygiene";
                             break;
-                    }                
-                    echo '<div class="kb-post" id="'.$id.'"  data-key="'.$key.'">
-                <h1 class="kb-post-title">Post title ' . ($i + 1)." ".$id.'</h1>
-                <div class="kb-post-info">
-                    <div class="kb-post-avatar">
-                        <i class="fa-solid fa-user"></i>
-                    </div>
-                    <div class="kb-post-topic">
-                        '.$key.'
-                    </div>
-                </div>
-                <div class="kb-post-divider"></div>
-                <p class="kb-post-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ' . ($i + 1) . '</p>
-              </div>';
+                    }
+                    echo '
+                    <div class="kb-post" id="' . $id . '"  data-key="' . $key . '">
+                        <h1 class="kb-post-title">
+                            Post title ' . ($i + 1) . " " . $id . '
+                        </h1>
+                        <div class="kb-post-info">
+                            <div class="kb-post-avatar">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                            <div class="kb-post-topic">
+                            ' . $key . '
+                            </div>
+                        </div>
+                        <div class="kb-post-divider"></div>
+                        <p class="kb-post-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ' . ($i + 1) . '</p>
+                    </div>';
                 }
                 ?>
             </div>
@@ -56,8 +59,8 @@
             <h3 class="kb-sidebar-title">Filter by Topic</h3>
             <ul id="topicsList">
                 <li class="kb-topic" id="codingStandards">Coding Standards</li>
-                <li class="kb-topic" id="printerIssues" >Printer Issues</li>
-                <li class="kb-topic" id="cybersecurity" >Cybersecurity</li>
+                <li class="kb-topic" id="printerIssues">Printer Issues</li>
+                <li class="kb-topic" id="cybersecurity">Cybersecurity</li>
                 <li class="kb-topic" id="workplaceHygiene">Workplace Hygiene</li>
             </ul>
             <button id="new-topic-btn">Add Topic</button>
