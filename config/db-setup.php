6838234f98fcfa821f142ac2f290a632e2f8cfc9
wookie184 <wookie1840@gmail.com>
2025-02-03 23:44:50 +0000

    Make indentation in setup file consistent

--- config/db-setup.php
+++ config/db-setup.php
@@ -2,11 +2,10 @@
 // Made by F328049 23/12/24
 // Moved and edited by F328049 02/01/2025
 
+include 'config.php';
 
-    include 'config.php';
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
