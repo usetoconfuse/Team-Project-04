01621384e2cfbe3c2a6fef290cc875fecbd86f37
Toby Tischler <toby.tischler@gmail.com>
2025-02-07 16:22:06 +0000

    Added custom scrollbar

--- login.php
+++ login.php
@@ -152,6 +152,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="login.css" />
+    <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         crossorigin="anonymous" />
 </head>
