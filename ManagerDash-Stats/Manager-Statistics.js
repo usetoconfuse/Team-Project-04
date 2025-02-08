74331e9a408708c1bb720fd4a390431f129063c7
Quinn <q.little-23@student.lboro.ac.uk>
2025-02-08 00:50:08 +0000

    Added query for User Stats Home Page - Testing Query Now

--- ManagerDash-Stats/Manager-Statistics.js
+++ ManagerDash-Stats/Manager-Statistics.js
@@ -57,7 +57,35 @@ mgrStatsPostTypeButtons.forEach(button => {
 
 
 
+//Get all Users query
+document.getElementById('mgrUserStats').addEventListener('click', async function() {
+    try {
+        // Make an HTTP request to the PHP file
+        const response = await fetch('userStatsPage-Queries/userStatsHomePageQuery.php');
+        console.log("1: ", response);
+        
+        // Ensure the response is OK and return the JSON data 
+        if (!response.ok) { 
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        // Convert the response to JSON format
+        const data = await response.json();
+        console.log("2: ", data[0].Forename);
 
+        // Find the container to display the data
+        // const container = document.getElementById('dataContainer');
+        // container.innerHTML = ''; // Clear any existing content
+
+        // // Loop through the data and create a new element for each item
+        // data.forEach(function(item) {
+        //     const div = document.createElement('div');
+        //     div.textContent = 'Number of projects which have tasks (starting from Project_ID 1): ' + item.projectCount;
+        //     container.appendChild(div); // Add the new element to the container
+        // });     
+    } catch (error) {
+        console.error('Error:', error); // Log any errors that occur
+    }
+});
 
 
 
