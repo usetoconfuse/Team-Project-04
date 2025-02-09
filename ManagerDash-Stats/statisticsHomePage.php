df8aa04a94edff5eb5a7a310894f949e0c2d9d41
Quinn <q.little-23@student.lboro.ac.uk>
2025-02-09 00:08:22 +0000

    -Added a grid in CSS for both the Project and User Home pages in Statistics, so that they are positioned nicely.

--- ManagerDash-Stats/statisticsHomePage.php
+++ ManagerDash-Stats/statisticsHomePage.php
@@ -1,6 +1,7 @@
 <!-- Updated by Quinn Little 03/02/25 -->
 <!-- Updated by Quinn Little 07/02/2025 -->
 <!-- Renamed by Quinn Little 07/02/2025 -->
+ <!-- Updated by Quinn Little 08/02/2025 -->
 
 <!-- Tabs -->
  <div id="mgrStatsAll">
@@ -15,30 +16,41 @@
         </div>
     </div>
 
-
-
+   
+<div id="statsHomeTable">
 <!-- Tab for Project Statistics -->
 <div id='tabGroupProjectStats'>
+<div id="statsHomeGridProject">
 
+    <div id="statsHomeFiltersProj"> 
+        Project Filters Here
+   </div>
+
+   <div id="statsHomeTableProj">
     <table id='projectStatsHomeTbl' class="statsHome-table"> <!--Fetch Project Data Here-->
     </table>
+</div>
+</div>
 
 </div>
 
 
 <!-- Tab for User Statistics -->
 <div id='tabGroupUserStats'>
+<div id="statsHomeGridUser">
 
-<table id='userStatsHomeTbl' class="statsHome-table"> <!--Fetch User Data Here-->
-</table>
+    <div id="statsHomeFilterUser"> 
+        User Filters Here
+   </div>
+   <div id="statsHomeTableUser">
+        <table id='userStatsHomeTbl' class="statsHome-table"> <!--Fetch User Data Here-->
+        </table>
+    </div>
 
 </div>
+</div>
 
-
-
-
-
-
+</div>
 
 
 
