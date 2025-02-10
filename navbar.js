21aaa6c024584b638add7cb5e4c95f485db91766
Toby Tischler <toby.tischler@gmail.com>
2025-02-10 10:05:48 +0000

    Disabled nav content scrolling, removed date and time navbar widget

--- navbar.js
+++ navbar.js
@@ -13,6 +13,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    /*
     //====Date and Time====//
     window.addEventListener("load", () => {
         time()
@@ -54,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
         setTimeout(date, 1000)
     }
-
+    */
  
 
     function getPageId() {
