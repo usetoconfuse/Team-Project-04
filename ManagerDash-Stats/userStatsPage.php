2d69808a7d7eb9cd27c277a94f8c222859ba9c29
Quinn <q.little-23@student.lboro.ac.uk>
2025-02-12 10:23:10 +0000

    - Overlap Graph Added

--- ManagerDash-Stats/userStatsPage.php
+++ ManagerDash-Stats/userStatsPage.php
@@ -62,3 +62,4 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 <script src="ManagerDash-Stats/userStatsPage.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
