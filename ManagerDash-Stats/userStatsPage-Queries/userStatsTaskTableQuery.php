9815a5b792e1bb83626e4d5d756f6277f556285d
Quinn <q.little-23@student.lboro.ac.uk>
2025-02-15 01:38:48 +0000

    -Added filters and Order By to the userStats page.

--- ManagerDash-Stats/userStatsPage-Queries/userStatsTaskTableQuery.php
+++ ManagerDash-Stats/userStatsPage-Queries/userStatsTaskTableQuery.php
@@ -1,153 +1,153 @@
 <?php 
 //Created by Quinn Little 08/02/2025
 
-    include '../../config/db-setup.php';
+    // include '../../config/db-setup.php';
 
-    // Check if ID parameter exists in the URL
-    if (isset($_GET['ID'])) {
-        // Retrieve parameter
-        $userID = htmlspecialchars($_GET['ID']); // Sanitize input
-        $stuck = htmlspecialchars($_GET['stuck']); // Sanitize input
-        $high = htmlspecialchars($_GET['high']); // Sanitize input
-        $earliest = htmlspecialchars($_GET['earliest']); // Sanitize input
-    }
+    // // Check if ID parameter exists in the URL
+    // if (isset($_GET['ID'])) {
+    //     // Retrieve parameter
+    //     $userID = htmlspecialchars($_GET['ID']); // Sanitize input
+    //     $stuck = htmlspecialchars($_GET['stuck']); // Sanitize input
+    //     $high = htmlspecialchars($_GET['high']); // Sanitize input
+    //     $earliest = htmlspecialchars($_GET['earliest']); // Sanitize input
+    // }
 
-    // $type = "Non-Technical"; // placeholder , will be fetched from button clicked
+    // // $type = "Non-Technical"; // placeholder , will be fetched from button clicked
 
-    // Select the first 20 tasks for the given UserID
-    $sql = "SELECT *, Tasks.Status AS 'Task_Status' 
-    FROM Tasks INNER JOIN Projects 
-    ON Tasks.Project_ID = Projects.Project_ID 
-    WHERE Tasks.Assignee_ID = '$userID'";
+    // // Select the first 20 tasks for the given UserID
+    // $sql = "SELECT *, Tasks.Status AS 'Task_Status' 
+    // FROM Tasks INNER JOIN Projects 
+    // ON Tasks.Project_ID = Projects.Project_ID 
+    // WHERE Tasks.Assignee_ID = '$userID'";
     
  
 
-    if ($stuck === '1') {
-        $sql .= " AND tasks.Stuck = '$stuck'";
-    }
-
-    if ($high === 'high') {
-        $sql .= " AND tasks.Priority = 'High'";
-    }
-
-    if ($earliest === 'earliest') {
-        $sql .= " ORDER BY tasks.Start_Date";
-    }
-    
-    // if ($type == "Technical"){
-	//     $sql .= " WHERE kb.Type = 'Technical' ";  
+    // if ($stuck === '1') {
+    //     $sql .= " AND tasks.Stuck = '$stuck'";
     // }
 
-    // if ($type == "Non-Technical"){
-    //     $sql .=  " WHERE kb.Type = 'Non-Technical' ";  
+    // if ($high === 'high') {
+    //     $sql .= " AND tasks.Priority = 'High'";
     // }
 
-    $result = mysqli_query($conn,$sql);
-
-    if(!$result){
-        die("Query failed ". mysqli_error($conn));
-    }
-
-    $allDataArray = array();
-    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        $allDataArray[] = $row;
-    }
+    // if ($earliest === 'earliest') {
+    //     $sql .= " ORDER BY tasks.Start_Date";
+    // }
     
-    echo json_encode($allDataArray);
+    // // if ($type == "Technical"){
+	// //     $sql .= " WHERE kb.Type = 'Technical' ";  
+    // // }
 
-    // include '../config/db-setup.php';
+    // // if ($type == "Non-Technical"){
+    // //     $sql .=  " WHERE kb.Type = 'Non-Technical' ";  
+    // // }
 
-// $userId = isset($_GET['userID']) ? intval($_GET['userID']) : null;
+    // $result = mysqli_query($conn,$sql);
 
-// $priority = isset($_GET['priorityValue']) ? $_GET['priorityValue'] : null;
-// $date = isset($_GET['dateValue']) ? $_GET['dateValue'] : null;
-// $stuck = isset($_GET['stuckValue']) ? $_GET['stuckValue'] : null;
-// $orderBy = isset($_GET['orderByValue']) ? $_GET['orderByValue'] : null;
+    // if(!$result){
+    //     die("Query failed ". mysqli_error($conn));
+    // }
 
-// $taskSQL = "SELECT t.Task_ID, t.Name, t.Description, t.Status, t.Due_Date, t.Start_Date, t.Priority, t.Author_ID, t.Stuck, t.Project_ID,
-//             t.Assignee_ID, p.Project_Title, u1.Forename AS User_Forename, u1.Surname AS User_Surname, u2.Forename AS Author_Forename, u2.Surname AS Author_Surname
-//             FROM Tasks t
-//             JOIN Projects p ON t.Project_ID = p.Project_ID
-//             LEFT JOIN Users u1 ON t.Assignee_ID = u1.User_ID
-//             LEFT JOIN Users u2 ON t.Author_ID = u2.User_ID
-//             WHERE t.Assignee_ID = $userId AND t.Start_Date <= CURDATE()";
+    // $allDataArray = array();
+    // while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    //     $allDataArray[] = $row;
+    // }
+    
+    // echo json_encode($allDataArray);
 
-// if (!empty($priority)) {
-//     $taskSQL .= " AND t.Priority = '$priority'";  
-// }
+    include '../../config/db-setup.php';
 
-// if (!empty($date)) {
-//     switch ($date) {
-//         case 'Today':
-//             $taskSQL .= " AND t.Due_Date = CURDATE()";
-//             break;
-//         case 'Tomorrow':
-//             $taskSQL .= " AND t.Due_Date = DATE_ADD(CURDATE(), INTERVAL 1 DAY)";
-//             break;
-//         case 'This Week':
-//             $taskSQL .= " AND YEARWEEK(t.Due_Date, 1) = YEARWEEK(CURDATE(), 1)";
-//             break;
-//         case 'Next Week':
-//             $taskSQL .= " AND t.Due_Date BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 WEEK)";
-//             break;
-//         case 'Later':
-//             $taskSQL .= " AND t.Due_Date > CURDATE()";
-//             break;
-//         default:
-//             break;
-//     }
-// }
+    $userId = isset($_GET['userID']) ? intval($_GET['userID']) : null;
 
-// if (!empty($stuck)) {
-//     switch ($stuck) {
-//         case 'Yes':
-//             $taskSQL .= " AND t.Stuck IN (1, 2)";
-//             break;
-//         case 'No':
-//             $taskSQL .= " AND t.Stuck = 0";
-//             break;
-//         default:
-//             break;
-//     }
-// }
+    $priority = isset($_GET['priorityValue']) ? $_GET['priorityValue'] : null;
+    $date = isset($_GET['dateValue']) ? $_GET['dateValue'] : null;
+    $stuck = isset($_GET['stuckValue']) ? $_GET['stuckValue'] : null;
+    $orderBy = isset($_GET['orderByValue']) ? $_GET['orderByValue'] : null;
 
-// if ($orderBy !== 'None') {
-//     if (!empty($orderBy)) {
-//         switch ($orderBy) {
-//             case 'Priority High to Low':
-//                 $taskSQL .= " ORDER BY t.Priority DESC";
-//                 break;
-//             case 'Priority Low to High':
-//                 $taskSQL .= " ORDER BY t.Priority ASC";
-//                 break;
-//             case 'Due First':
-//                 $taskSQL .= " ORDER BY t.Due_Date ASC";
-//                 break;
-//             case 'Due Last':
-//                 $taskSQL .= " ORDER BY t.Due_Date DESC";
-//                 break;
-//             case 'Most Overdue':
-//                 $taskSQL .= " ORDER BY t.Due_Date ASC";
-//                 break;
-//             default: 
-//                 break;
-//         }
-//     }
-// }
+    $taskSQL = "SELECT t.Task_ID, t.Name, t.Description, t.Status, t.Due_Date, t.Start_Date, t.Priority, t.Author_ID, t.Stuck, t.Project_ID,
+                t.Assignee_ID, p.Project_Title, u1.Forename AS User_Forename, u1.Surname AS User_Surname, u2.Forename AS Author_Forename, u2.Surname AS Author_Surname
+                FROM Tasks t
+                JOIN Projects p ON t.Project_ID = p.Project_ID
+                LEFT JOIN Users u1 ON t.Assignee_ID = u1.User_ID
+                LEFT JOIN Users u2 ON t.Author_ID = u2.User_ID
+                WHERE t.Assignee_ID = $userId AND t.Start_Date <= CURDATE()";
+
+    if (!empty($priority)) {
+        $taskSQL .= " AND t.Priority = '$priority'";  
+    }
+
+    if (!empty($date)) {
+        switch ($date) {
+            case 'Today':
+                $taskSQL .= " AND t.Due_Date = CURDATE()";
+                break;
+            case 'Tomorrow':
+                $taskSQL .= " AND t.Due_Date = DATE_ADD(CURDATE(), INTERVAL 1 DAY)";
+                break;
+            case 'This Week':
+                $taskSQL .= " AND YEARWEEK(t.Due_Date, 1) = YEARWEEK(CURDATE(), 1)";
+                break;
+            case 'Next Week':
+                $taskSQL .= " AND t.Due_Date BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 WEEK)";
+                break;
+            case 'Later':
+                $taskSQL .= " AND t.Due_Date > CURDATE()";
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (!empty($stuck)) {
+        switch ($stuck) {
+            case 'Yes':
+                $taskSQL .= " AND t.Stuck IN (1, 2)";
+                break;
+            case 'No':
+                $taskSQL .= " AND t.Stuck = 0";
+                break;
+            default:
+                break;
+        }
+    }
+
+    if ($orderBy !== 'None') {
+        if (!empty($orderBy)) {
+            switch ($orderBy) {
+                case 'Priority High to Low':
+                    $taskSQL .= " ORDER BY t.Priority DESC";
+                    break;
+                case 'Priority Low to High':
+                    $taskSQL .= " ORDER BY t.Priority ASC";
+                    break;
+                case 'Due First':
+                    $taskSQL .= " ORDER BY t.Due_Date ASC";
+                    break;
+                case 'Due Last':
+                    $taskSQL .= " ORDER BY t.Due_Date DESC";
+                    break;
+                case 'Most Overdue':
+                    $taskSQL .= " ORDER BY t.Due_Date ASC";
+                    break;
+                default: 
+                    break;
+            }
+        }
+    }
 
 
-// $taskResult = mysqli_query($conn, $taskSQL);
+    $taskResult = mysqli_query($conn, $taskSQL);
 
-// if (!$taskResult) {
-//     die("Query failed for Task Results" . mysqli_error($conn));
-// }
+    if (!$taskResult) {
+        die("Query failed for Task Results" . mysqli_error($conn));
+    }
 
-// $taskDataArray = array();
-// while ($row = mysqli_fetch_array($taskResult, MYSQLI_ASSOC)) {
-//     $taskDataArray[] = $row;
-// }
+    $taskDataArray = array();
+    while ($row = mysqli_fetch_array($taskResult, MYSQLI_ASSOC)) {
+        $taskDataArray[] = $row;
+    }
 
-// echo json_encode($taskDataArray);
+    echo json_encode($taskDataArray);
 
 ?>
 
