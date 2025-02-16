b91b678512148fe6b2952089dee5b924711d2c89
wookie184 <wookie1840@gmail.com>
2025-02-16 00:51:06 +0000

    Order topics by popularity

--- knowledgeBase/query/getTopics.php
+++ knowledgeBase/query/getTopics.php
@@ -18,6 +18,13 @@ if ($query !== null) {
     $sql .= " AND LOWER(Topic_Name) LIKE LOWER('%$query%')";
 }
 
+$sql .= " 
+ORDER BY (
+    SELECT Count(Post_ID) 
+    FROM Post_Topic 
+    WHERE Topic_ID = Topics.Topic_ID
+) DESC";
+
 $result = mysqli_query($conn, $sql);
 $allDataArray = array();
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
